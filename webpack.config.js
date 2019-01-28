90fdd354b1a5e4ecf8210e91b3655179387086df
Emilio <thecraftedgem@tuta.io>
2019-01-28 03:10:16 -0700

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ module.exports = {
 
   output: {
     path: __dirname +  "/dist",
-    filename: "[name].bundle.js"
+    filename: "[name].bundle.js",
+    resolve: {
+      extensions: ['.js', '.jsx']
+    }
   }
 };
