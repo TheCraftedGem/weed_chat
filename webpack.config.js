bc7a49ac4163841845bf0f57787784727da6602d
Emilio <thecraftedgem@tuta.io>
2019-01-28 02:35:33 -0700

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,8 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var CopyWebpackPlugin = require("copy-webpack-plugin");
-
 module.exports = {
-  entry: ["./web/static/css/app.css", "./web/static/js/app.js"],
+  entry: ["./priv/static/js/app.js"],
 
   output: {
-    path: "./priv/static",
-    filename: "js/app.js"
-  },
-
-  resolve: {
-    modulesDirectories: ["node_modules", __dirname + "/web/static/js"]
-  },
-
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel",
-      query: {
-        presets: ["es2015"]
-      }
-    }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract("style", "css")
-    }]
-  },
-
-  plugins: [
-    new ExtractTextPlugin("css/app.css"),
-    new CopyWebpackPlugin([{
-      from: "./web/static/assets"
-    }])
-  ]
+    path: __dirname +  "/dist",
+    filename: "app.js"
+  }
 };
