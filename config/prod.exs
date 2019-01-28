f296075c8e6d0519cf2a941a59cd2b50607dcbba
Emilio <thecraftedgem@tuta.io>
2019-01-27 19:08:45 -0700

    working on deployment

--- config/prod.exs
+++ config/prod.exs
@@ -21,11 +21,11 @@ config :weed_chat, WeedChatWeb.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 
-  config :weed_chat, WeedChat.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+config :weed_chat, WeedChat.Repo,
+adapter: Ecto.Adapters.Postgres,
+url: System.get_env("DATABASE_URL"),
+pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+ssl: true
 
 
 # Do not print debug messages in production
